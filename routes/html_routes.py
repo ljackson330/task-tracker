d77f0ed32d9a82b7bb0e532a5388a9810fa2353d
Liam <153872676+ljackson330@users.noreply.github.com>
2024-05-16 22:36:02 -0700

    v0.1 - Base [RELEASE] (#25)
    #25

--- routes/html_routes.py
+++ routes/html_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, abort
+from db import db
 from models import User, Task
 
 html_routes_bp = Blueprint('html_routes', __name__)
@@ -11,7 +12,8 @@ Routes allow you to define Flask pages in a logical and organized way
 @html_routes_bp.route("/")
 def index():
     """Renders the home page under /templates/index.html"""
-    return render_template("index.html")
+    tasks = Task.query.all()
+    return render_template("index.html", tasks=tasks)
 
 
 @html_routes_bp.route("/tasks")
@@ -24,3 +26,48 @@ def tasks():
     """
     tasks = Task.query.all()
     return render_template("tasks.html", tasks=tasks)
+
+
+@html_routes_bp.route("/tasks/create", methods=["GET", "POST"])
+def create_tasks():
+    """
+    Creates a task object using the POST request body
+    """
+    if request.method == "POST":
+        task_title = request.form["title"]
+        task_description = request.form["description"]
+        db.session.add(Task(name=task_title, desc=task_description, user_id=1))
+        db.session.commit()
+        return redirect(url_for('html_routes.tasks'))
+    return render_template("create.html")
+
+
+@html_routes_bp.route("/tasks/<int:task_id>/delete", methods=["POST"])
+def delete_task(task_id):
+    """
+    Deletes a task from the database based on the provided task ID
+    """
+    task = db.session.get(Task, task_id)
+    if task is None:
+        abort(404)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("html_routes.tasks"))
+
+
+@html_routes_bp.route("/tasks/<int:task_id>/edit", methods=["POST"])
+def edit_task(task_id):
+    """
+    Edits a task from the database based on the provided task ID
+    """
+    if request.method == "POST":
+        task = db.session.get(Task, task_id)
+        if task is None:
+            abort(404)
+        task_description = request.form["description"]
+        task_title = request.form["title"]
+        task.name = task_title
+        task.desc = task_description
+        db.session.commit()
+        return redirect(url_for("html_routes.tasks"))
+    return redirect(url_for("html_routes.tasks"))
